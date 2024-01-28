e9110b0d9263a082dcbe249583dd30a4d7356731
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-01-28 15:34:59 +0200

    Tailwind CSS class sorting with Prettier and Volar takeover mode

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# on-track
+# Time Tracker
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -2,6 +2,8 @@
 import { currentPage, navigate } from '../router'
 import { scrollToCurrentHour } from '../timeline-items'
 import { PAGE_TIMELINE } from '../constants'
+import { ICON_CLOCK } from '../icons'
+import BaseIcon from './BaseIcon.vue'
 
 function handleClick() {
   currentPage.value === PAGE_TIMELINE ? scrollToCurrentHour(true) : navigate(PAGE_TIMELINE)
@@ -9,7 +11,7 @@ function handleClick() {
 </script>
 
 <template>
-  <a :href="`#${PAGE_TIMELINE}`" @click="handleClick">
-    <img src="../assets/img/logo.png" alt="Logo" class="h-9" />
+  <a :href="`#${PAGE_TIMELINE}`" @click="handleClick" class="bg-black text-white p-1 rounded scale-125 origin-left">
+    <BaseIcon :name="ICON_CLOCK" class="h-5" />
   </a>
 </template>
